47f0f615d96fe0f8b18591546f7f015958cd49ec
Jet-Kane <jetkane@gmail.com>
2019-10-31 12:48:40 +0800

    Initial Commit

---
+++ UdderB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UdderB
+//
+//  Created by JetKane on 2019/10/31.
+//  Copyright © 2019 JetKane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
